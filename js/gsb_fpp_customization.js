6efba7f8f92c742b15f753816b4fdc184b614baa
Greg Mercer <gmercer@stanford.edu>
2013-06-05 10:34:49 -0600

    A little code cleanup.

--- js/gsb_fpp_customization.js
+++ js/gsb_fpp_customization.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.gsbfppcustomization = {
     attach: function(context, settings) {
 
-      // initialize superfish menus
-      var example = $('#add-fpp-content-menu').superfish({
+      // initialize superfish for the add fpp content menu
+      $('#add-fpp-content-menu').superfish({
         animation: {height:'show'},   // slide-down effect without fade-in
         delay:     1000               // 1.2 second delay on mouseout
       });

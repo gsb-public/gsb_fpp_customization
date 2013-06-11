0d39bab8c1a9e3310fada3a89534931c2afcc51a
Greg Mercer <gmercer@stanford.edu>
2013-06-11 11:50:18 -0600

    Part of WP-1575 Added code to support 'back' and 'cancel' links.

--- js/gsb_fpp_customization.js
+++ js/gsb_fpp_customization.js
@@ -2,10 +2,26 @@
   Drupal.behaviors.gsbfppcustomization = {
     attach: function(context, settings) {
 
+      // hide the cancel button, which functionally is replaced by
+      // the 'back' and 'cancel' links
+      $('#edit-cancel').hide();
+
       // initialize superfish for the add fpp content menu
       $('#add-fpp-content-menu').superfish({
         animation: {height:'show'},   // slide-down effect without fade-in
         delay:     1000               // 1.2 second delay on mouseout
+      });
+
+      // trigger the 'back' link via a cancel button click
+      $('#back-link').click(function(event) {
+        event.preventDefault();
+        $('#edit-cancel').click();
+      });
+
+      // trigger the 'cancel' link via a cancel button click
+      $('#cancel-link').click(function(event) {
+        event.preventDefault();
+        $('#edit-cancel').click();
       });
         
     }

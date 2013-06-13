caccadad5dc08a1c693af6bfd8715557a7b6222e
Greg Mercer <gmercer@stanford.edu>
2013-06-13 09:13:53 -0600

    WP-1575 Had to add some 'special' getting the cancel button element.

--- js/gsb_fpp_customization.js
+++ js/gsb_fpp_customization.js
@@ -2,9 +2,21 @@
   Drupal.behaviors.gsbfppcustomization = {
     attach: function(context, settings) {
 
+      // todo: Would really like to change how this works.
+      // ok... wow this is wild. 
+      // Drupal for some reason renames the cancel button each time
+      // the form is submitted. So I had to resort to this method to 
+      // find the cancel button. 
+      var cancelButton = null;
+      $('.form-submit').each(function(index) {
+        if ($(this).val() == 'Cancel') {
+          cancelButton = $(this);
+        }
+      });
+
       // hide the cancel button, which functionally is replaced by
       // the 'back' and 'cancel' links
-      $('#edit-cancel').hide();
+      cancelButton.hide();
 
       // initialize superfish for the add fpp content menu
       $('#add-fpp-content-menu').superfish({
@@ -15,15 +27,16 @@
       // trigger the 'back' link via a cancel button click
       $('#back-link').click(function(event) {
         event.preventDefault();
-        $('#edit-cancel').click();
+        cancelButton.click();
       });
 
       // trigger the 'cancel' link via a cancel button click
       $('#cancel-link').click(function(event) {
         event.preventDefault();
-        $('#edit-cancel').click();
+        cancelButton.click();
       });
         
     }
   }
 })(jQuery);
+

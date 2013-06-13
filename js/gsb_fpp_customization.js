0bbe8cc845044cda05301c6bc9f8fdb72dd21a4f
Greg Mercer <gmercer@stanford.edu>
2013-06-13 10:24:25 -0600

    Cleaned up this code a bit.

--- js/gsb_fpp_customization.js
+++ js/gsb_fpp_customization.js
@@ -2,22 +2,6 @@
   Drupal.behaviors.gsbfppcustomization = {
     attach: function(context, settings) {
 
-      // todo: Would really like to change how this works.
-      // ok... wow this is wild. 
-      // Drupal for some reason renames the cancel button each time
-      // the form is submitted. So I had to resort to this method to 
-      // find the cancel button. 
-      var cancelButton = null;
-      $('.form-submit').each(function(index) {
-        if ($(this).val() == 'Cancel') {
-          cancelButton = $(this);
-        }
-      });
-
-      // hide the cancel button, which functionally is replaced by
-      // the 'back' and 'cancel' links
-      cancelButton.hide();
-
       // initialize superfish for the add fpp content menu
       $('#add-fpp-content-menu').superfish({
         animation: {height:'show'},   // slide-down effect without fade-in
@@ -25,18 +9,11 @@
       });
 
       // trigger the 'back' link via a cancel button click
-      $('#back-link').click(function(event) {
+      $('#back-link, #cancel-link').click(function(event) {
         event.preventDefault();
-        cancelButton.click();
-      });
-
-      // trigger the 'cancel' link via a cancel button click
-      $('#cancel-link').click(function(event) {
-        event.preventDefault();
-        cancelButton.click();
+        $('.gsb-panopoly-magic-customization-cancel-button').click();
       });
         
     }
   }
 })(jQuery);
-
